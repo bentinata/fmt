befd23703cccff42599f04ec6e6a16851e36f44e
Ben Nata <benget.nata@aia.com>
2022-07-01 16:58:05 +0800

    Evaluate extends before exports

--- src/eslint.js
+++ src/eslint.js
@@ -1,6 +1,31 @@
-module.exports = {
+const baseConfig = {
   extends: [
     "eslint:recommended",
     "prettier",
   ],
+  rules: {
+  },
+  env: {
+    node: true
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
 };
+
+const _ = require("lodash");
+const { FlatCompat } = require("@eslint/eslintrc");
+const compat = new FlatCompat({ baseDirectory: __dirname });
+const { extends: _extends, rules: _rules, ...configWithoutRulesAndExtend } = baseConfig;
+
+// hack so main repo doesn't need to install eslint-config-*
+const config = _.merge(configWithoutRulesAndExtend, ...compat.config(baseConfig).map(c => {
+  if (typeof c === "string") {
+    return { extends: c };
+  }
+  if (c.rules) {
+    return { rules: c.rules };
+  }
+}));
+
+module.exports = config;
