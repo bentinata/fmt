b1ecb5087b29cd506f0311c330ab8d3d15534581
Ben Nata <benget.nata@aia.com>
2022-07-03 10:41:10 +0800

    Pretty prints

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const match = process.version.match(/v(\d+)\.(\d+)/)
+const match = process.version.match(/v(\d+)\.(\d+)/);
 const major = Number(match[1]);
 
 if (major < 16) {
@@ -13,6 +13,7 @@ const minimist = require("minimist");
 const argv = minimist(process.argv.slice(2));
 const patterns = argv._.length === 0 ? ["."] : argv._.map(String);
 const expandPatterns = require("./expandPatterns");
+const color = require("picocolors");
 
 const prettier = require("prettier");
 const prettierConfig = require("../src/prettier");
@@ -21,10 +22,39 @@ const eslintConfig = require("../src/eslint");
 const { ESLint } = require("eslint");
 const eslint = new ESLint({ fix: true, baseConfig: eslintConfig });
 
-(async function() {
+(async function () {
   for await (const path of expandPatterns(patterns)) {
     const input = await fs.readFile(path, "utf-8");
-    const prettierOut = prettier.format(input, { filepath: path, ...prettierConfig });
-    await fs.writeFile(path, prettierOut, "utf-8");
+    const startTime = Date.now();
+
+    const prettierOut = prettier.format(input, {
+      filepath: path,
+      ...prettierConfig,
+    });
+    const prettierTime = Date.now();
+    const prettierStatus =
+      (prettierOut === input
+        ? color.gray("prettier:none")
+        : color.green("prettier:done")) +
+      color.gray(` ${prettierTime - startTime}ms`);
+
+    const [eslintOut] = await eslint.lintText(prettierOut, { filePath: path });
+    const eslintTime = Date.now();
+    const eslintStatus =
+      (eslintOut.errorCount > 0
+        ? color.red("eslint:err")
+        : eslintOut.warningCount > 0
+        ? color.yellow("eslint:warn")
+        : eslintOut.output
+        ? color.green("eslint:done")
+        : color.gray("eslint:none")) +
+      color.gray(` ${eslintTime - prettierTime}ms`);
+
+    const output = eslintOut.output ?? prettierOut;
+    if (output !== input) {
+      await fs.writeFile(path, prettierOut, "utf-8");
+    }
+
+    console.log(path, prettierStatus, eslintStatus);
   }
 })();
