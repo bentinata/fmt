b8df333e53590ae4180f5409341925b0ab1baa1b
Ben Nata <benget.nata@aia.com>
2022-07-03 09:31:03 +0800

    Clarity with package name and comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,5 @@
-module.exports = require("./").eslint;
+/*
+ * This rc file is or development purposes,
+ * i.e. to test eslint on this repo.
+ */
+module.exports = require(".").eslint;
---
+++ .prettierrc.js
@@ -0,0 +1,5 @@
+/*
+ * This rc file is or development purposes,
+ * i.e. to test prettier on this repo.
+ */
+module.exports = require(".").prettier;
